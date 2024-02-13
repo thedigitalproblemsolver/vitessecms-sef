a0658dacab835db2a06700263bdfd685a9fa1a46
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2024-02-13 21:23:49 +0100

    Added bytespider and tests

--- src/Utils/SefUtil.php
+++ src/Utils/SefUtil.php
@@ -7,23 +7,22 @@ namespace VitesseCms\Sef\Utils;
 use Phalcon\Di\Di;
 use VitesseCms\Core\Utils\UrlUtil;
 
-class SefUtil
+final class SefUtil
 {
     public static function fixSlashes(string $slug): string
     {
-        $file = str_replace('//', '/', Di::getDefault()->get('config')->get('webDir') . $slug);
+        $file = str_replace('//', '/', Di::getDefault()->get('config')->get('webDir').$slug);
         $urlExists = false;
 
-        if (Di::getDefault()->get('config')->get('importUrl')) :
-            $url = Di::getDefault()->get('config')->get('importUrl') . $slug;
+        if (Di::getDefault()->get('config')->get('importUrl')) {
+            $url = Di::getDefault()->get('config')->get('importUrl').$slug;
             $urlExists = UrlUtil::exists($url);
-        endif;
+        }
 
-        if (!is_file($file) && !$urlExists) :
-            $slug = rtrim($slug, '/') . '/';
-            $slug = '/' . ltrim($slug, '/');
-        endif;
-
+        if (!is_file($file) && !$urlExists) {
+            $slug = rtrim($slug, '/').'/';
+            $slug = '/'.ltrim($slug, '/');
+        }
 
         return $slug;
     }
@@ -33,14 +32,15 @@ class SefUtil
         $userAgent = strtolower($userAgent);
 
         return
-            str_contains($userAgent, 'googlebot') ||
-            str_contains($userAgent, 'google-structured-data-testing-tool') ||
-            str_contains($userAgent, 'bingbot') ||
-            str_contains($userAgent, 'msnbot') ||
-            str_contains($userAgent, 'ahrefsbot') ||
-            str_contains($userAgent, 'google-inspectiontool') ||
-            str_contains($userAgent, 'applebot') ||
-            str_contains($userAgent, 'bitsightbot');
+            str_contains($userAgent, 'googlebot')
+            || str_contains($userAgent, 'google-structured-data-testing-tool')
+            || str_contains($userAgent, 'bingbot')
+            || str_contains($userAgent, 'msnbot')
+            || str_contains($userAgent, 'ahrefsbot')
+            || str_contains($userAgent, 'google-inspectiontool')
+            || str_contains($userAgent, 'applebot')
+            || str_contains($userAgent, 'bitsightbot')
+            || str_contains($userAgent, 'bytespider');
     }
 
     public static function generateSlugFromString(string $string): string
